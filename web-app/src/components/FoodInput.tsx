8a3c67841f495b14b9ca1b785ea66e2a3e58fbe6
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-10-27 12:53:23 -0400

    change to table format

--- web-app/src/components/FoodInput.tsx
+++ web-app/src/components/FoodInput.tsx
@@ -7,12 +7,22 @@ export interface FoodInputProps {
 }
 
 export const FoodInput = (props: FoodInputProps) => {
+    
     return (
         <div>
-            <input id="food" type="text" value={props.food}/>
-            <input id="food_group" type="text" value={props.foodGroup}/>
-            <input id="calories" type="text" value={props.caloriesPer100g}/>
-            <button>Add</button>
+            <table>
+                <tr>
+                    <th>Food</th>
+                    <th>Food Group</th>
+                    <th>Calries per 100g</th>
+                </tr>
+                <tr>
+                    <td><input type="text" value={props.food}/></td>
+                    <td><input type="text" value={props.foodGroup}/></td>
+                    <td><input type="text" value={props.caloriesPer100g}/></td>
+                    <td><button>Add</button></td>
+                </tr>
+            </table>
         </div>
     );
 
