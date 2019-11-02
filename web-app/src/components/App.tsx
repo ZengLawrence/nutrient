cc7cd3a73eb4206ad39a2a5243038287afabfa48
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-02 12:30:01 -0400

    add input as modal, not fully formatted yet

--- web-app/src/components/App.tsx
+++ web-app/src/components/App.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { FoodInput } from "./FoodInput";
 import { FoodList } from "./FoodList";
 import { FoodCalorie } from '../models/FoodCalorie';
@@ -17,9 +17,26 @@ export const App = (props: AppProps) => {
   const AddFoodCalorie = (foodCalorie: FoodCalorie) => {
     setFoodCalories([...foodCalories, foodCalorie]);
   }
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div>
-      <FoodInput addFoooCalorie={AddFoodCalorie} />
+      <Button color="primary" onClick={toggle}>Add</Button>
+
+      <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>Add Calorie</ModalHeader>
+        <ModalBody>
+          <FoodInput addFoooCalorie={AddFoodCalorie} />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Add</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+
       <FoodList foodCalories={foodCalories} />
     </div>
   );
