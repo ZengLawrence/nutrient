7903ec7d84dfdb2ce13b7d06452a1ee01686c5a4
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-03 14:38:36 -0500

    change to modal input for add

--- web-app/src/components/App.tsx
+++ web-app/src/components/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { FoodInput } from "./FoodInput";
+
 import { FoodList } from "./FoodList";
 import { FoodCalorie } from '../models/FoodCalorie';
+import { FoodInput } from './FoodInput';
 
 export interface AppProps {
   foodCalories?: FoodCalorie[];
@@ -14,29 +14,13 @@ export const App = (props: AppProps) => {
 
   const [foodCalories, setFoodCalories] = useState(emptyFoodCalories);
 
-  const AddFoodCalorie = (foodCalorie: FoodCalorie) => {
-    setFoodCalories([...foodCalories, foodCalorie]);
+  const addFoodCalorie = (foodCalorie: FoodCalorie) => {
+      setFoodCalories([...foodCalories, foodCalorie]);
   }
-
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
 
   return (
     <div>
-      <Button color="primary" onClick={toggle}>Add</Button>
-
-      <Modal isOpen={modal} toggle={toggle} >
-        <ModalHeader toggle={toggle}>Add Calorie</ModalHeader>
-        <ModalBody>
-          <FoodInput addFoooCalorie={AddFoodCalorie} />
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>Add</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
-      </Modal>
-
+      <FoodInput onFoodCalorieChanged={addFoodCalorie}/>
       <FoodList foodCalories={foodCalories} />
     </div>
   );
--- web-app/src/components/FoodInput.tsx
+++ web-app/src/components/FoodInput.tsx
@@ -1,42 +1,55 @@
 import * as React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { FoodCalorie } from "../models/FoodCalorie";
-import { Button } from 'reactstrap';
+import { Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export interface FoodInputProps extends FoodCalorie { 
-    addFoooCalorie: (foodcalorie: FoodCalorie) => void
+export interface FoodInputProps {
+    onFoodCalorieChanged: (foodcalorie: FoodCalorie) => void
 }
 
 export const FoodInput = (props: FoodInputProps) => {
 
-    const [food, setFood] = useState(props.food);
-    const [foodGroup, setFoodGroup] = useState(props.foodGroup);
-    const [caloriesPer100g, setCaloriesPer100g] = useState(props.caloriesPer100g);
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    const [food, setFood] = useState("");
+    const [foodGroup, setFoodGroup] = useState("");
+    const [caloriesPer100g, setCaloriesPer100g] = useState(0);
 
     const addFoodCalorie = () => {
-        let foodCalorie : FoodCalorie = {food: food, foodGroup: foodGroup, caloriesPer100g: caloriesPer100g};
-        props.addFoooCalorie(foodCalorie);
+        let foodCalorie: FoodCalorie = { food: food, foodGroup: foodGroup, caloriesPer100g: caloriesPer100g };
+        props.onFoodCalorieChanged(foodCalorie);
     }
 
     return (
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Food</th>
-                        <th>Food Group</th>
-                        <th>Calries per 100g</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type="text" defaultValue={props.food} onChange={(e) => setFood(e.target.value)}/></td>
-                        <td><input type="text" defaultValue={props.foodGroup} onChange={(e) => setFoodGroup(e.target.value)} /></td>
-                        <td><input type="text" defaultValue={props.caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseFloat(e.target.value))} /></td>
-                        <td><Button color="primary" onClick={() => addFoodCalorie()}>Add</Button></td>
-                    </tr>
-                </tbody>
-            </table>
+            <Button color="primary" onClick={toggle}>Add</Button>
+
+            <Modal isOpen={modal} toggle={toggle} >
+                <ModalHeader toggle={toggle}>Add Calorie</ModalHeader>
+                <ModalBody>
+                <Label for="foodGroup">Food Group</Label>
+                <Input type="select" id="foodGroup" key="foodGroup" value={foodGroup} onChange={(e) => setFoodGroup(e.target.value)}>
+                    <option>Vegetable</option>
+                    <option>Fruit</option>
+                    <option>Carbohydrate</option>
+                    <option>Potein</option>
+                    <option>Fat</option>
+                    <option>Sweet</option>
+                </Input>
+                <br />
+                <Label for="food">Food</Label>
+                <Input placeholder="Orange" id="food" key="food" value={food} onChange={(e) => setFood(e.target.value)} />
+                <br />
+                <Label for="calorie">Calories per 100g</Label>
+                <Input type="number" id="calorie" key="calorie" value={caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={addFoodCalorie}>Add</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
         </div>
     );
 
