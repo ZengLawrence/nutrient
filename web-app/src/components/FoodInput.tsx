b421fb1b03ec8edccb12b2a093743bb18ab4c83c
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-03 14:49:01 -0500

    sorted food group lsit

--- web-app/src/components/FoodInput.tsx
+++ web-app/src/components/FoodInput.tsx
@@ -3,6 +3,14 @@ import { useState } from "react";
 import { FoodCalorie } from "../models/FoodCalorie";
 import { Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+const foodGroups = ["Vegetable",
+    "Fruit",
+    "Carbohydrate",
+    "Potein",
+    "Fat",
+    "Sweet",
+];
+
 export interface FoodInputProps {
     onFoodCalorieChanged: (foodcalorie: FoodCalorie) => void
 }
@@ -29,21 +37,16 @@ export const FoodInput = (props: FoodInputProps) => {
             <Modal isOpen={modal} toggle={toggle} >
                 <ModalHeader toggle={toggle}>Add Calorie</ModalHeader>
                 <ModalBody>
-                <Label for="foodGroup">Food Group</Label>
-                <Input type="select" id="foodGroup" value={foodGroup} onChange={(e) => setFoodGroup(e.target.value)}>
-                    <option>Vegetable</option>
-                    <option>Fruit</option>
-                    <option>Carbohydrate</option>
-                    <option>Potein</option>
-                    <option>Fat</option>
-                    <option>Sweet</option>
-                </Input>
-                <br />
-                <Label for="food">Food</Label>
-                <Input placeholder="Orange" id="food" value={food} onChange={(e) => setFood(e.target.value)} />
-                <br />
-                <Label for="calorie">Calories per 100g</Label>
-                <Input type="number" id="calorie" value={caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
+                    <Label for="foodGroup">Food Group</Label>
+                    <Input type="select" id="foodGroup" value={foodGroup} onChange={(e) => setFoodGroup(e.target.value)}>
+                        {foodGroups.sort().map( (fg) => <option>{fg}</option>)}
+                    </Input>
+                    <br />
+                    <Label for="food">Food</Label>
+                    <Input placeholder="Orange" id="food" value={food} onChange={(e) => setFood(e.target.value)} />
+                    <br />
+                    <Label for="calorie">Calories per 100g</Label>
+                    <Input type="number" id="calorie" value={caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={addFoodCalorie}>Add</Button>{' '}
