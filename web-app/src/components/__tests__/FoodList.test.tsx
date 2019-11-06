1a6aea5f2a2e497305e254780ab29dc3874c2dae
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-06 08:47:22 -0500

    test food calorie list with 3 items

--- web-app/src/components/__tests__/FoodList.test.tsx
+++ web-app/src/components/__tests__/FoodList.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { FoodList } from '../FoodList';
+import { FoodCalorie } from '../../models/FoodCalorie';
 
 it('renders empty food calorie list', () => {
   const { asFragment } = render(<FoodList foodCalories={[]} />);
   expect(asFragment()).toMatchSnapshot();
 });
+
+it('renders food calorie list with 3 food items', () => {
+    const foodCalories: FoodCalorie[] = [
+        {food: "Orange", foodGroup: "Fruit", caloriesPer100g: 47},
+        {food: "Sliced Bread", foodGroup: "Carbohydrate", caloriesPer100g: 90},
+        {food: "Chicken Breast", foodGroup: "Protein", caloriesPer100g: 110},
+    ];
+    const { asFragment } = render(<FoodList foodCalories={foodCalories} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
