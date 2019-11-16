5ed7d078e4dbc8bb562756b98eb3505035391a2c
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-16 08:23:59 -0500

    add food calorie api call

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {Response, Request} from 'express';
 import { FoodCalorie } from '../models/FoodCalorie';
 
-const foodCalories: FoodCalorie[] = [
+let foodCalories: FoodCalorie[] = [
     {foodGroup: 'Vegetable', food: 'Broccoli', caloriesPer100g: 35},
     {foodGroup: 'Fruit', food: 'Orange', caloriesPer100g: 47},
     {foodGroup: 'Carbohydrate', food: 'Sliced Bread', caloriesPer100g: 110},
@@ -10,10 +10,16 @@ const foodCalories: FoodCalorie[] = [
 
 export const register = (router: Router) => {
     router.get( '/api/food-calories', foodCaloriesRoute );
+    router.post('/api/food-calories', addFoodCalorie);
 
     return router;
 };
 
 const foodCaloriesRoute = (req: Request, resp: Response) => {
     resp.json( {foodCalories: foodCalories} );
+};
+
+const addFoodCalorie = (req: Request, resp: Response) => {
+    const foodCalorie: FoodCalorie = req.body;
+    foodCalories = [...foodCalories, foodCalorie];
 };
--- web-app/src/services/FoodCalorieService.ts
+++ web-app/src/services/FoodCalorieService.ts
@@ -1,9 +1,17 @@
+import { AxiosResponse } from 'axios';
 import { FoodCalorie } from '../models/FoodCalorie';
+
+const axios = require('axios').default;
 
 export const fetchAllFoodCalories = (callback: (foodCalories: FoodCalorie[]) => void) => {
 
-    fetch('/api/food-calories')
-        .then((resp) => resp.json())
-        .then((data) => callback(data.foodCalories));
+    axios.get('/api/food-calories')
+        .then(function (resp: AxiosResponse<{foodCalories: FoodCalorie[]}>) {
+            callback(resp.data.foodCalories);
+        });
 
+};
+
+export const addFoodCalorie = (foodCalorie: FoodCalorie) => {
+    axios.post('/api/food-calories', foodCalorie);
 };
