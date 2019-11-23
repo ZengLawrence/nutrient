24f1b718b1eb9e70abd94880bf59c10e49432abc
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-11-23 10:06:07 -0500

    add input validation

--- client/src/components/FoodInput.tsx
+++ client/src/components/FoodInput.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { FoodCalorie } from '../models/FoodCalorie';
-import { Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Label, Input, Button, Modal, ModalHeader, ModalBody, FormGroup, Form } from 'reactstrap';
 
 const foodGroups = ['Vegetable',
     'Fruit',
@@ -31,26 +31,41 @@ export const FoodInput = (props: FoodInputProps) => {
         props.onFoodCalorieChanged(foodCalorie);
     };
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === true) {
+            addFoodCalorie();
+        }
+
+        // do not close modal window
+        event.preventDefault();
+        event.stopPropagation();
+    };
+  
     return (
         <div>
             <Modal isOpen={isOpen} toggle={toggle} >
                 <ModalHeader toggle={toggle}>Add Calorie</ModalHeader>
                 <ModalBody>
-                    <Label for="foodGroup">Food Group</Label>
-                    <Input type="select" id="foodGroup" value={foodGroup} onChange={(e) => setFoodGroup(e.target.value)}>
-                        {foodGroups.sort().map( (fg) => <option key={fg}>{fg}</option>)}
-                    </Input>
-                    <br />
-                    <Label for="food">Food</Label>
-                    <Input placeholder="Broccoli" id="food" value={food} onChange={(e) => setFood(e.target.value)} />
-                    <br />
-                    <Label for="calorie">Calories per 100g</Label>
-                    <Input type="number" id="calorie" value={caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <Label for="foodGroup">Food Group</Label>
+                            <Input type="select" id="foodGroup" value={foodGroup} onChange={(e) => setFoodGroup(e.target.value)}>
+                                {foodGroups.sort().map((fg) => <option key={fg}>{fg}</option>)}
+                            </Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="food">Food</Label>
+                            <Input required placeholder="Broccoli" id="food" value={food} onChange={(e) => setFood(e.target.value)} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="calorie">Calories per 100g</Label>
+                            <Input type="number" id="calorie" value={caloriesPer100g} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
+                        </FormGroup>
+                        <Button color="primary" type='submit'>Add</Button>{' '}
+                        <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    </Form>
                 </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={addFoodCalorie}>Add</Button>{' '}
-                    <Button color="secondary" onClick={toggle}>Cancel</Button>
-                </ModalFooter>
             </Modal>
         </div>
     );
