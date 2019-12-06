8bf9015b7bd874ca259e14bec04c98c1f7d1371e
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-06 07:44:23 -0500

    remove routing

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AddFoodCalorieFormContainer } from './AddFoodCalorieFormContainer';
 import { Home } from './Home';
 
 export const App = (props: {}) => {
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/food-calorie">
-          <AddFoodCalorieFormContainer />
-        </Route>
-      </Switch>
-    </Router>
+    <Home />
   );
 };
