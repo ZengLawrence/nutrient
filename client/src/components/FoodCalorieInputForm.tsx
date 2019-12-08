82b457fe50503c2845b7b798322366b546cb8f01
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-08 11:49:05 -0500

    reformat input form

--- client/src/components/FoodCalorieInputForm.tsx
+++ client/src/components/FoodCalorieInputForm.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Form, FormGroup, Input, Label, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { Col, Form, FormGroup, Input, Label, Row } from 'reactstrap';
+
+const ENTER_KEY = 13;
 
 export interface FoodCalorieInputFormProps {
     add: (food: string, caloriesPer100g: number) => void,
@@ -21,26 +23,33 @@ export const FoodCalorieInputForm = (props: FoodCalorieInputFormProps) => {
         } else {
             event.stopPropagation();
         }
+    }
 
-    };
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.keyCode === ENTER_KEY) {
+            add(food, caloriesPer100g);
+            setFood('');
+            setCaloriesPer100g(0);
+        }
+    }
 
     return (
-        <div>
-            <Form inline onSubmit={handleSubmit}>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="food" className="mr-sm-2">Food</Label>
-                    <Input required placeholder="Broccoli" id="food" value={food} onChange={(e) => setFood(e.target.value)} />
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                    <Label for="calorie" className="mr-sm-2">Calories</Label>
-                    <Input type="number" id="calorie" value={caloriesPer100g} className="mr-sm-1" onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
-                    <InputGroupAddon addonType="append">
-                        <InputGroupText>/100g</InputGroupText>
-                    </InputGroupAddon>
-                </FormGroup>
-                <Button color="primary" type='submit'>Add</Button>
-            </Form>
-        </div>
+        <Form>
+            <Row form>
+                <Col sm={8}>
+                    <FormGroup>
+                        <Label for="food">Food</Label>
+                        <Input required placeholder="Broccoli" id="food" value={food} onKeyDown={handleKeyDown} onChange={(e) => setFood(e.target.value)} />
+                    </FormGroup>
+                </Col>
+                <Col sm={4}>
+                    <FormGroup>
+                        <Label for="calorie">Calories/100g</Label>
+                        <Input type="number" id="calorie" value={caloriesPer100g} className="col-sm-6" onKeyDown={handleKeyDown} onChange={(e) => setCaloriesPer100g(parseInt(e.target.value))} />
+                    </FormGroup>
+                </Col>
+            </Row>
+        </Form>
     );
 
 };
