4d7dd62ddfd4d7ab2b38869a64f56e8350a2296f
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-08 10:04:29 -0500

    format home page as a card

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,9 +5,22 @@ import VisibleFoodCalorieList from '../containers/VisibleFoodCalorieList';
 export const Home = () => {
 
   return (
-    <div>
-      <AddFoodCalorie />
-      <VisibleFoodCalorieList />
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="col-md-6">
+          <div className="card shadow-sm p-3 mt-3 mb-5 rounded">
+            <div className="card-body">
+
+              <h4 className="card-title text-center">Food Calories</h4>
+              <AddFoodCalorie />
+              <VisibleFoodCalorieList />
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 };
