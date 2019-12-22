90691667c586f331d86cda6abb4f3419c592a536
Lawrence Zeng <lawrence.zeng@gmail.com>
2019-12-22 10:41:12 -0500

    return id field

--- src/models/FoodCalorie.ts
+++ src/models/FoodCalorie.ts
@@ -6,11 +6,12 @@ export interface IFoodCalorie extends mongoose.Document {
     caloriesPer100g: number
 }
 
-export const FoodCalorieSchema = new mongoose.Schema({
+const foodCalorieSchema = new mongoose.Schema({
     food: { type: String, required: true},
     foodGroup: { type: String, required: true},
     caloriesPer100g: { type: Number, required: true}
 });
+foodCalorieSchema.set('toJSON', { virtuals: true });
 
-const FoodCalorie = mongoose.model<IFoodCalorie>("FoodCalorie", FoodCalorieSchema);
+const FoodCalorie = mongoose.model<IFoodCalorie>("FoodCalorie", foodCalorieSchema);
 export default FoodCalorie;
